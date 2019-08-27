5acabf81e3aecbbf4dad53d8658129848d4759c2
Manuel Ojeda <manuelojedasistemas@gmail.com>
2019-08-26 18:11:27 -0600

    Initial Config

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
